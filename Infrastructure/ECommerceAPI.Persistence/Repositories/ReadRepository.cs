336f0356455b1a2b88124fa122d9cf1f35907969
HP <HP@LAPTOP-7L7IFFSP>
2024-10-08 21:07:04 +0300

    repository design pattern has been applied.

---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities.Common;
+using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ECommerceAPI.Persistence.Repositories;
+
+public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+{
+	private readonly ECommerceAPIDbContext context;
+	public ReadRepository(ECommerceAPIDbContext context)
+	{
+		this.context = context;
+	}
+	public DbSet<T> Table => context.Set<T>();
+
+	public IQueryable<T> GetAll()
+		=> Table;
+
+	public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+		=> Table.Where(method);
+
+	public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+		=> await Table.FirstOrDefaultAsync(method);
+
+	public async Task<T> GetByIdAsync(string id)
+		=> await Table.FindAsync(Guid.Parse(id));
+
+}
--- Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -1,11 +1,8 @@
-﻿using ECommerceAPI.Persistence.Contexts;
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Persistence.Contexts;
+using ECommerceAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerceAPI.Persistence;
 
@@ -14,5 +11,11 @@ public static class ServiceRegistration
 	public static void AddPersistenceServices(this IServiceCollection services)
 	{
 		services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+		services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+		services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+		services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+		services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+		services.AddScoped<IProductReadRepository, ProductReadRepository>();
+		services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
 	}
 }
